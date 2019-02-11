21a923f3d7e9a51c8e81bdcc82470636f4a670be
Todd Boyce Jr <tjboyce@me.com>
2019-02-11 14:36:33 -0600

    base complete

--- database.sql
+++ database.sql
@@ -1,3 +1,16 @@
 -- 1. SELECT * FROM "accounts" WHERE "city" = 'chicago';
 --2. SELECT * FROM "accounts" WHERE "username" ILIKE '%a%';
---3. 
+--3. SELECT * FROM "accounts" WHERE "account_balance" = 0 
+--AND "transactions_attempted" = 0;
+--4. SELECT * FROM "accounts" WHERE "transactions_attempted" >9;
+--5. SELECT "username", "account_balance" FROM "accounts"
+--ORDER BY "account_balance" DESC LIMIT 3; 
+--6. SELECT "username" , "account_balance" FROM "accounts"
+--ORDER BY "account_balance" ASC LIMIT 3;
+--7. SELECT * FROM "accounts" WHERE "account_balance" > 100.00;
+--8. INSERT INTO accounts (username, city, transactions_completed, transactions_attempted, account_balance) VALUES
+--('Tj', 'minneapolis', 10, 10, 1400);
+--9. DELETE FROM "accounts" 
+--WHERE ("city" = 'miami' OR "city" ='phoenix') 
+--AND "transactions_completed" < 5;
+
